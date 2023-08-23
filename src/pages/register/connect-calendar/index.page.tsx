d65949283b56aba3db804653fb3c0ac8ada8a78f
Alex Moreira <alex.moreira.andrade@hotmail.com>
2023-08-22 23:22:23 -0300

    feat: page calendar connection

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -1,11 +1,11 @@
-import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
+import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
-import { api } from '@/lib/axios'
-import { Container, Form, Header } from '../styles'
+// import { api } from "../../../lib/axios"
+import { Container, Header } from '../styles'
+import { ConnectBox, ConnectItem } from './styles'
 
 export default function Register() {
-
-  // async function handleRegister(data: RegisterFormData) {
+  // async function handleRegister() {
 
   // }
 
@@ -15,18 +15,26 @@ export default function Register() {
         <Heading as="strong">Conecte sua agenda!</Heading>
         <Text>
           Conecte o seu calendário para verificar automaticamente as horas
-          ocupadas e os novos eventos a medida em que são agendados
+          ocupadas e os novos eventos à medida em que são agendados.
         </Text>
 
         <MultiStep size={4} currentStep={2} />
       </Header>
 
-      <Button type="submit" >
-        Próximo passo
-        <ArrowRight />
-      </Button>
+      <ConnectBox>
+        <ConnectItem>
+          <Text>Google Calendar</Text>
+          <Button variant="secondary" size="sm">
+            Conectar
+            <ArrowRight />
+          </Button>
+        </ConnectItem>
 
-
+        <Button type="submit">
+          Próximo passo
+          <ArrowRight />
+        </Button>
+      </ConnectBox>
     </Container>
   )
 }
