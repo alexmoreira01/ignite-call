032857eb26924ae6b6b78bdcdd7af2654f309cc5
Alex Moreira <alex.moreira.andrade@hotmail.com>
2023-10-01 20:01:11 -0300

    feat: navigation and calc for month

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,5 +1,10 @@
+import { useMemo, useState } from 'react'
+
 import { CaretLeft, CaretRight } from 'phosphor-react'
+import dayjs from 'dayjs'
+
 import { getWeekDays } from '../../utils/get-week-days'
+
 import {
   CalendarActions,
   CalendarBody,
@@ -9,21 +14,74 @@ import {
   CalendarTitle,
 } from './styles'
 
+interface CalendarWeek {
+  week: number
+  days: Array<{
+    date: dayjs.Dayjs
+    disabled: boolean
+  }>
+}
+
+type CalendarWeeks = CalendarWeek[]
+
 export function Calendar() {
+  const [currentDate, setCurrentDate] = useState(() => {
+    return dayjs().set('date', 1) // Pegando dia 1 do mes
+  })
+
+  function handlePreviousMonth() {
+    const previousMonthDate = currentDate.subtract(1, 'month')
+
+    setCurrentDate(previousMonthDate)
+  }
+
+  function handleNextMonth() {
+    const previousMonthDate = currentDate.add(1, 'month')
+
+    setCurrentDate(previousMonthDate)
+  }
+
   const shortWeekDays = getWeekDays({ short: true })
+
+  const currentMonth = currentDate.format('MMMM') // Mes por extenso
+  const currentYear = currentDate.format('YYYY') // Ano por extenso
+
+  // Array.from aqui eles estao criados arrays sem valores somente com o index
+  const calendarWeeks = useMemo(() => {
+    const daysInMonthArray = Array.from({
+      length: currentDate.daysInMonth(),
+    }).map((_, i) => {
+      return currentDate.set('date', i + 1)
+    })
+
+    // Dia da semana do primeiro dia do mes / day == dia da semana / date é o dia
+    const firstWeekDay = currentDate.get('day') // Sempre ira retornar quantos dias faltaram para preencher a linha da semana
+
+    const previousMonthFillArray = Array.from({
+      length: firstWeekDay,
+    })
+      .map((_, i) => {
+        return currentDate.subtract(i + 1, 'day') // Pegando a data e voltado os dias da ultima semana do ultimo mes
+      })
+      .reverse()
+
+    return [...previousMonthFillArray, ...daysInMonthArray]
+  }, [currentDate])
+
+  console.log(calendarWeeks)
 
   return (
     <CalendarContainer>
       <CalendarHeader>
         <CalendarTitle>
-          Dezembro <span>2022</span>
+          {currentMonth} <span>{currentYear}</span>
         </CalendarTitle>
 
         <CalendarActions>
-          <button>
+          <button onClick={handlePreviousMonth} title="Previous month">
             <CaretLeft />
           </button>
-          <button>
+          <button onClick={handleNextMonth} title="Next month">
             <CaretRight />
           </button>
         </CalendarActions>
