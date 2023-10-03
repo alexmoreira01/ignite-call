16da89ace5ec48b5d75f5892e3e2969e1869b72e
Alex Moreira <alex.moreira.andrade@hotmail.com>
2023-10-03 00:01:40 -0300

    feat: routes for available times

--- src/pages/api/users/[username]/availability.api.ts
+++ src/pages/api/users/[username]/availability.api.ts
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 import dayjs from 'dayjs'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { prisma } from '../../../../lib/prisma'
@@ -12,6 +13,8 @@ export default async function handler(
 
   const username = String(req.query.username)
   const { date } = req.query
+
+  // http://localhost:3333/api/users/alex-moreira/availability?date=2023-10-04
 
   if (!date) {
     return res.status(400).json({ message: 'Date no provided.' })
@@ -28,7 +31,7 @@ export default async function handler(
   }
 
   const referenceDate = dayjs(String(date))
-  const isPastDate = referenceDate.endOf('day').isBefore(new Date())
+  const isPastDate = referenceDate.endOf('day').isBefore(new Date()) // Se a data ja passou
 
   if (isPastDate) {
     return res.json({ availability: [] })
@@ -47,12 +50,13 @@ export default async function handler(
 
   const { time_start_in_minutes, time_end_in_minutes } = userAvailability
 
-  const startHour = time_start_in_minutes / 60
-  const endHour = time_end_in_minutes / 60
+  const startHour = time_start_in_minutes / 60 // 10 -> retorno / Convertendo o time_start que esta em minutos para horas
+  const endHour = time_end_in_minutes / 60 // 18 -> retorno
 
+  // [10, 11, 12, 13, 14, 15, 16, 17] => Horas disponiveis que devem ser retornadas
   const possibleTimes = Array.from({ length: endHour - startHour }).map(
     (_, i) => {
-      return startHour + i
+      return startHour + i // Mesmo não alterando os dados para retornar, ele ira percorrer o array e para cada posiçao ira crir uma hora adicionaondo mais 1
     },
   )
 
