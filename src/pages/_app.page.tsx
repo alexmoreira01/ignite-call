9a65fbe5af29b28bd1406d732674ea5b45102b83
Alex Moreira <alex.moreira.andrade@hotmail.com>
2023-10-15 17:04:43 -0300

    feat: Events Google and SEO

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -5,6 +5,7 @@ import { globalStyles } from '@/styles/global'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from '@/lib/react-query'
 import type { AppProps } from 'next/app'
+import { DefaultSeo } from 'next-seo'
 
 globalStyles() // Carregamos uma unica vez, caso fique no componente ira carregar toda vez
 
@@ -15,6 +16,15 @@ export default function App({
   return (
     <QueryClientProvider client={queryClient}>
       <SessionProvider session={session}>
+        <DefaultSeo
+          openGraph={{
+            type: 'website',
+            locale: 'pt_BR',
+            url: 'https://www.ignite-call.rocketseat.com.br',
+            siteName: 'Ignite Call',
+          }}
+        />
+
         <Component {...pageProps} />
       </SessionProvider>
     </QueryClientProvider>
